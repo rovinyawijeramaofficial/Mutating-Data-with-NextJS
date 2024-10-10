3d057a955d136fc0819b80c1318214a944862650
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-10 10:48:05 +0530

    Updated

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata={
+//   title:'Browse All Our X Posts.',
+//   description:'Browse our latest posts!'
+// };
+
+export async function generateMetadata(){
+  const posts =await getPosts();
+  const numberOfPosts=posts.length;
+  return{
+    title:`Browse All Our ${numberOfPosts} Posts.`,
+    description:'Browse our latest posts!'
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
